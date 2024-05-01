61dc47c285b4820448a9f4022abe9bc0349b3470
Liam Oldfield <liamoldfield@hotmail.co.uk>
2024-05-01 23:25:31 +0100

    gitignore more, and start server script

---
+++ start.sh
@@ -0,0 +1,7 @@
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+pip install --upgrade Flask
+pip install --upgrade MarkupSafe
+pip install --upgrade Jinja2
+python3 app.py
